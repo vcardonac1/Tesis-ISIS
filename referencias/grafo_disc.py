b1b931e240eda3cd995951fd484a58756151c3e9
Vhanessa <v.cardonac1@uniandes.edu.co>
2022-11-22 11:42:41 -0500

    grafos

--- referencias/grafo_disc.py
+++ referencias/grafo_disc.py
@@ -10,12 +10,17 @@ class grafo:
             self.estructura = g.newGraph(size=10)
 
     def addNode_byValue(self, infoNodo):    #done
-        self.estructura = g.insertVertex(self.estructura, infoNodo)
+        infoNodo = str(infoNodo)
+        if not g.containsVertex(self.estructura, infoNodo):
+            self.estructura = g.insertVertex(self.estructura, infoNodo)
 
     def addEdge_byValue(self, infoNodo_1, infoNodo_2, weight=0): #done
+        infoNodo_1 = str(infoNodo_1)
+        infoNodo_2 = str(infoNodo_2)
         self.estructura = g.addEdge(self.estructura, infoNodo_1, infoNodo_2, weight)
 
     def deleteNode_byValue(self, infoNodo): #done
+        infoNodo = str(infoNodo)
         self.estructura = g.removeVertex(self.estructura, infoNodo)
 
     def deleteEdge_byValue(self, infoNodo_1, infoNodo_2):
@@ -39,9 +44,11 @@ class grafo:
         return lst
 
     def isNodeValue(self, infoNodo):        #done
+        infoNodo = str(infoNodo)
         return g.containsVertex(self.estructura, infoNodo)
 
     def findAdjacentNode(self, infoNodo):   #done
+        infoNodo = str(infoNodo)
         lst = list()
         iter = lt.iterator(g.adjacents(self.estructura, infoNodo))
         for i in iter:
