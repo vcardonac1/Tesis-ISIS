36d86dc17cc50d4abf99264517ed7621adb3eee8
Vhanessa <v.cardonac1@uniandes.edu.co>
2022-12-06 11:33:52 -0500

    documentacion

--- referencias/grafo_disc.py
+++ referencias/grafo_disc.py
@@ -13,31 +13,89 @@ from DISClib.Algorithms.Graphs import dijsktra as djk
 from DISClib.ADT import queue as q
 
 class grafo:
-    def __init__(self, type='Undirected'):               # done
+    '''
+    Clase que representa una Grafo
+    
+    Por facilidad del uso de esta clase en el Proyecto, todos los valores de los nodos se
+    tomarán como Strings. Pero se puede poder user la implementación con cualquier tipo
+    de dato 
+    
+    '''
+    def __init__(self, type='Undirected'):
+        '''
+        Inicializacion del grafo. Crea una grafo vacio del tipo especificado
+        
+        Args:
+            type: Directed o Undirected
+        Returns:
+            -
+            
+        '''
         if type == 'Directed':
             self.estructura = g.newGraph(size=10, directed=True)
         else:
             self.estructura = g.newGraph(size=10)
 
-    def addNode_byValue(self, infoNodo):    #done
+    def addNode_byValue(self, infoNodo):
+        '''
+        Añade un nodo al grafo dado su valor, el nodo se añade al grafo.
+        Si el nodo ya existe no se añade
+        
+        Args:
+            infoNodo: Información del nodo para añadir a la lista
+        
+        Returns:
+            -
+            
+        '''
         infoNodo = str(infoNodo)
         if not g.containsVertex(self.estructura, infoNodo):
             self.estructura = g.insertVertex(self.estructura, infoNodo)
 
-    def addEdge_byValue(self, infoNodo_1, infoNodo_2, weight=0): #done
+    def addEdge_byValue(self, infoNodo_1, infoNodo_2, weight=0):
+        '''
+        Añade un arco al grafo dado valor del nodo origen, nodo destino y peso.
+        El arco se añade al grafo.
+                
+        Args:
+            infoNodo_1: Información del nodo origen
+            infoNodo_2: Información del nodo destino
+        
+        Returns:
+            -
+        
+        Raises:
+            Exception si alguno de los nodos no pertenece al grafo
+        '''
         infoNodo_1 = str(infoNodo_1)
         infoNodo_2 = str(infoNodo_2)
         self.estructura = g.addEdge(self.estructura, infoNodo_1, infoNodo_2, weight)
 
-    def deleteNode_byValue(self, infoNodo): #done
+    def deleteNode_byValue(self, infoNodo):
+        '''
+        Elimina un nodo del grafo dado valor.
+                
+        Args:
+            infoNodo: Información del nodo que se va a eliminar
+        
+        Returns:
+            -
+
+        '''
         infoNodo = str(infoNodo)
         self.estructura = g.removeVertex(self.estructura, infoNodo)
 
-    def deleteEdge_byValue(self, infoNodo_1, infoNodo_2):
-        #TODO
-        pass
+    def getEdgeValues(self):
+        '''
+        Devuelve los valores de todos los arcos del grafo
+                
+        Args:
+            -
+        
+        Returns:
+            Lista (python) de tuplas de la forma (nodo_origen, nodo_destino, peso)
 
-    def getEdgeValues(self):                #done
+        '''
         lst = list()
         iter = lt.iterator(g.edges(self.estructura))
         aux = ()
@@ -46,18 +104,48 @@ class grafo:
             lst.append(aux)
         return lst            
     
-    def getNodeValues(self):                #done
+    def getNodeValues(self):
+        '''
+        Devuelve los valores de todos los nodos del grafo
+                
+        Args:
+            -
+        
+        Returns:
+            Lista (python) de los valores de los nodos del grafo
+
+        '''
         lst = list()
         iter = lt.iterator(g.vertices(self.estructura))
         for i in iter:
             lst.append(i)
         return lst
 
-    def isNodeValue(self, infoNodo):        #done
+    def isNodeValue(self, infoNodo):
+        '''
+        Informa si un nodo pertenece o no al grafo
+        
+        Args:
+            infoNodo: Valor del nodo que se busca
+        
+        Returns:
+            True si el nodo pertenece al grafo, False si no pertenece
+            
+        '''
         infoNodo = str(infoNodo)
         return g.containsVertex(self.estructura, infoNodo)
 
-    def findAdjacentNode(self, infoNodo):   #done
+    def findAdjacentNode(self, infoNodo):
+        '''
+        Devuelve los valores de los nodos que son adyacentes al nodo del valor dado
+        
+        Args:
+            infoNodo: Valor del nodo del que se buscan los adyacentes
+        
+        Returns:
+            Lista (python) con los valores de los nodos adyacentes al nodo dado. Si no tiene adyacentes se retorna una lista vacia
+            
+        '''
         infoNodo = str(infoNodo)
         lst = list()
         try:
@@ -69,6 +157,31 @@ class grafo:
         return lst
     
     def algorithms(self, algoritmo, infoNodo=None):
+        '''
+        Función que ejecuta un algoritmo dado en el grafo
+        
+        Args:
+            algoritmo: Nombre del algoritmo que se va a ejecutar: Bellman-Ford, BreadhtFirstSearch, DirectedCycle,  
+            DepthFirstSearch, DepthFirstOrder, PrimMST, KosarajuSCC, Dijkstra
+        
+        Returns:
+            Dependiendo del algoritmo:
+                DepthFirstSearch, BreadhtFirstSearch, DepthFirstOrder': retornan una lista (python) de los valores de los nodos
+                
+                DirectedCycle: retorna una lista (python) con tuplas de arcos de la forma (nodo_origen, nodo_destino)
+                
+                Dijkstra, Bellman-Ford: retorna una lista de diccionarios (python) con keys: node, path, cost
+                    - node: valor del nodo
+                    - cost: costo de ir del nodo infoNodo a node
+                    - path: lista (python) de tuplas de arcos de la forma (nodo_origen, nodo_destino) que indican el camino para ir de infoNodo a node
+                
+                KosarajuSCC: retorna un diccionario donde cada llave indica una componente fuertemente conectada, y cada valor es una lista (python) de los
+                    valores de los nodos que pertenecen a dicha componente
+                
+                PrimMST: retorna una tupla (edges, weight) donde edges es una lista (python) de arcos en forma tuplas (nodo_origen, nodo_destino)
+                        y weight es el costo total de los arcos mencionados
+        
+        '''
         if infoNodo != None:
             infoNodo = str(infoNodo)
         table = list()
--- referencias/lista_disc.py
+++ referencias/lista_disc.py
@@ -1,18 +1,21 @@
 import config
 from DISClib.ADT import list as lt
 
-def defaultfunction(id1, id2):
-    if type(id1) != type(id2):
-        id1 = str(id1)
-        id2 = str(id2)
-    if id1 > id2:
-        return 1
-    elif id1 < id2:
-        return -1
-    return 0
-
 class listaEnlazada():
+    '''
+    Clase que representa una Lista Enlazada
+    '''
     def __init__(self, type):
+        '''
+        Inicializacion de la lista enlazada. Crea una lista enlazada vacia del tipo especificado
+        
+        Args:
+            type: 1 si la lista es sencilla, 2 si la lista es doble
+
+        Returns:
+            -
+            
+        '''
         self.type = type
         if type == 1: 
             self.estructura = lt.newList(datastructure='SINGLE_LINKED', cmpfunction=defaultfunction)
@@ -20,9 +23,33 @@ class listaEnlazada():
             self.estructura = lt.newList(datastructure='DOUBLE_LINKED', cmpfunction=defaultfunction)
     
     def addNode_byValue(self, infoNodo):
+        '''
+        Añade un nodo a la lista enlazada, el nodo se añade al final de la lista.
+        Si el nodo ya se encuentra en la lista, se añade una nueva instancia
+        
+        Args:
+            infoNodo: Información del nodo para añadir a la lista
+        
+        Returns:
+            -
+            
+        '''
         lt.addLast(self.estructura, infoNodo)
     
     def deleteNode_byValue(self, infoNodo):
+        '''
+        Elimina un nodo de la lista enlazada.
+            Si el nodo no existe se retorna False
+            
+            Si hay mas de un nodo con el valor indicado, solo se elimina una instancia.
+        
+        Args:
+            infoNodo: Información del nodo que se va a eliminar
+        
+        Returns:
+            False si el nodo no pertenece a la lista, True si el nodo se elimina de la lista
+            
+        '''
         pos = lt.isPresent(self.estructura, infoNodo)
         if pos > 1:
             lt.deleteElement(self.estructura, pos)
@@ -34,6 +61,16 @@ class listaEnlazada():
             return False
     
     def getNodeValues(self):
+        '''
+        Da todos los elementos de la lista en el orden que se tienen, iniciando por el primero
+        
+        Args:
+            -
+        
+        Returns:
+            Lista (python) con todos los valores de los nodos, en el orden del primero al ultimo
+
+        '''
         lst = list()
         iter = lt.iterator(self.estructura)
         for i in iter:
@@ -41,12 +78,34 @@ class listaEnlazada():
         return lst
     
     def isNodeValue(self, infoNodo):
+        '''
+        Informa si un nodo pertenece o no a la lista enlazada
+        
+        Args:
+            infoNodo: Valor del nodo que se busca
+        
+        Returns:
+            True si el nodo pertenece a la lista enlazada, False si no pertenece
+            
+        '''
         if lt.isPresent(self.estructura, infoNodo) != 0:
             return True
         else:
             return False
     
     def findAdjacentNode(self, infoNodo):
+        '''
+        Devuelve los valores de los nodos que son adyacentes a un nodo dado. En el caso de la lista enlazada sencilla, 
+        el adyacente de un nodo es el siguiente, y en el caso de una lista enlazada doble, los adyacentes son
+        el siguiente y el anterior
+        
+        Args:
+            infoNodo: Información del nodo del que se buscan los adyacentes
+        
+        Returns:
+            Lista (python) con los valores de los nodos adyacentes al nodo dado. Si no tiene adyacentes se retorna una lista vacia
+            
+        '''
         lst = list()
         pos = lt.isPresent(self.estructura, infoNodo)
         size = lt.size(self.estructura)
@@ -56,3 +115,26 @@ class listaEnlazada():
             if pos+1 <= size:
                 lst.append(lt.getElement(self.estructura, pos+1))
         return lst
+
+def defaultfunction(elem_1, elem_2):
+    '''
+    Función de comparación entre dos elementos. Si son del mismo tipo de dato se comparan directamente, 
+    de lo contrario se transforman ambos elementos a String y se realiza la comparación
+    
+    Args:
+        elem_1: Elemento 1. 
+        elem_2: Elemento 2.
+    
+    Returns:
+        -1: Si el elemento 1 es menor al elemento 2
+         0: Si el elemento 1 es igual al elemento 2
+         1: Si el elemento 1 es mayor al elemento 2
+    '''
+    if type(elem_1) != type(elem_2):
+        elem_1 = str(elem_1)
+        elem_2 = str(elem_2)
+    if elem_1 > elem_2:
+        return 1
+    elif elem_1 < elem_2:
+        return -1
+    return 0
